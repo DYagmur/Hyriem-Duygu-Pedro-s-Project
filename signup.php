dbdaa5d6bcc796f88b1dc9762a5ac993eec9baaf
Duygu Yagmur <30656517+DYagmur@users.noreply.github.com>
2023-06-04 14:30:53 +0300

    signup fixing v5

--- signup.php
+++ signup.php
@@ -1,8 +1,12 @@
 <?php
 
-require_once("inc/Page.class.php");
-require_once("inc/PageContent.class.php");
+require_once("inc/config.inc.php");
+require_once("inc/Entities/User.class.php");
+require_once("inc/Utilities/PDOService.class.php");
+require_once("inc/Utilities/DAO/UserDAO.class.php");
 require_once("inc/Utilities/LoginManager.class.php");
+require_once("inc/Page.class.php");
+
 
 session_start();
 LoginManager::verifyLogin();
@@ -10,25 +14,47 @@ UserDAO::startDb();
 
 $currentUser = $_SESSION["username"];
 echo Page::PageHeader();
-if ( ! empty($_POST)) {
-    $newUser = new User();
-    $newUser->setUsername($_POST['userName']);
-    $newUser->setEmail($_POST['email']);
-    $newUser->setUserPicture($_POST['userPicture']);
 
-    $newPass = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    $newUser->setPassword($newPass);
-
-    $userExist = UserDAO::getUserByUsername($_POST['userName']);
- 
-    if (!$userExist) {
-        UserDAO::insertUser($newUser);  
-        echo Page::successMessage();
-        unset($_POST);
+function signup($username, $email, $password)
+{
+    // Validate input parameters
+    if (empty($username) || empty($email) || empty($password)) {
+        return "Please provide all the required information.";
     }
+
+    // Check if the user already exists in the database
+    $userExist = UserDAO::getUserByUsername($username);
+    if ($userExist) {
+        return "Username already exists. Please choose a different username.";
+    }
+
+    // Create a new User instance
+    $newUser = new User();
+    $newUser->setUsername($username);
+    $newUser->setEmail($email);
+    $newUser->setUserPicture('default.jpg'); // Default user picture
+
+    // Hash the password
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    $newUser->setPassword($hashedPassword);
+
+    // Insert the new user into the database
+    UserDAO::insertUser($newUser);
+
+    // Successful signup
+    return "Signup successful!";
 }
 
-echo Page::pageHeader();
-echo Page::titleDefault("Hi there, nice to see you!");
-echo Page::formSignup();
-echo Page::pageFooter();
+// Handle form submission
+if (!empty($_POST)) {
+    $username = $_POST['userName'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $result = signup($username, $email, $password);
+
+    // Display the result
+    echo $result;
+}
+
+
